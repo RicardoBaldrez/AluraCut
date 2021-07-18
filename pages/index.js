d58ad793a74c6ebc5d3d5e1a4e0205fa9d2b6982
Ricardo Barbosa Baldrez <ricardo.baldrez@gmail.com>
2021-07-18 15:36:01 -0300

    Separando a imagem do profile em uma FunctionComponent e passando para ele uma props, que leva a qual foto de usuário trazer

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 
+function ProfileSidebar(props) {
+  return(
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+} 
+
 export default function Home() {
   const githubUser = 'RicardoBaldrez';
 
   return (
     <MainGrid>
       <div className="profileArea" style={{ gridArea: "profileArea" }}>
-        <Box>
-          <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
-        </Box>
+        <ProfileSidebar githubUser={githubUser} />
       </div>
       <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
         <Box>Bem Vindo</Box>
