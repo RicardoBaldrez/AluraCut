55dba0251e78564a263dbdd1380d95fea28bfc89
Ricardo Barbosa Baldrez <ricardo.baldrez@gmail.com>
2021-07-18 15:47:00 -0300

    Trazendo lib(Alura) para nossa aplicação e usando o componente de menu

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,38 @@
-import MainGrid from '../src/components/MainGrid';
-import Box from '../src/components/Box';
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import { AlurakutMenu } from "../src/lib/AlurakutCommons";
 
 function ProfileSidebar(props) {
-  return(
+  return (
     <Box>
-      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
     </Box>
-  )
-} 
+  );
+}
 
 export default function Home() {
-  const githubUser = 'RicardoBaldrez';
+  const githubUser = "RicardoBaldrez";
 
   return (
-    <MainGrid>
-      <div className="profileArea" style={{ gridArea: "profileArea" }}>
-        <ProfileSidebar githubUser={githubUser} />
-      </div>
-      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
-        <Box>Bem Vindo</Box>
-      </div>
-      <div className="profileRelationsArea" style={{ gridArea: "profileRelationsArea" }}>
-        <Box>Pessoas da comunidade</Box>
-      </div>
-    </MainGrid>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>Bem Vindo</Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <Box>Pessoas da comunidade</Box>
+        </div>
+      </MainGrid>
+    </>
   );
 }
